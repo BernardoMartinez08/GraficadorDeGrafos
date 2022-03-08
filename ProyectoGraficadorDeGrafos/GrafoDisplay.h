29ae5f5663b16c906e268f97e111e9e90eadd317
BernardoMartinez08 <bernardo.martinez@unitec.edu>
2022-03-08 02:02:20 -0600

    panel

--- ProyectoGraficadorDeGrafos/GrafoDisplay.h
+++ ProyectoGraficadorDeGrafos/GrafoDisplay.h
@@ -34,6 +34,9 @@ namespace ProyectoGraficadorDeGrafos {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Panel^ panel1;
 
 	private:
 		/// <summary>
@@ -48,16 +51,41 @@ namespace ProyectoGraficadorDeGrafos {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe Fluent Icons", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::MidnightBlue;
+			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(229, 21);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"GRAFICADOR DE GRAFOS.";
+			// 
+			// panel1
+			// 
+			this->panel1->Location = System::Drawing::Point(360, 12);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(960, 619);
+			this->panel1->TabIndex = 1;
 			// 
 			// GrafoDisplay
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(746, 599);
+			this->BackColor = System::Drawing::Color::SkyBlue;
+			this->ClientSize = System::Drawing::Size(1332, 643);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->label1);
 			this->Name = L"GrafoDisplay";
 			this->Text = L"GrafoDisplay";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
